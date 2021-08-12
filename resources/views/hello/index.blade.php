c8cc0bd0efe30a83b278cc200f67d08da67df0e6
shoi <tash0531@icloud.com>
2021-08-12 10:37:34 +0900

    Register for validation error message

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,12 +9,21 @@
 
 @section('content')
     <p>{{$msg}}</p>
+    @if (count($errors) > 0)
+        <div>
+            <ul>
+                @foreach ($errors->all() as $errors)
+                    <li>{{$errors}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form action="/hello" method="post">
         <table>
             @CSRF
-            <tr><th>name: </th><td><input type="text" name="name"></td></tr>
-            <tr><th>mail: </th><td><input type="mail" name="mail"></td></tr>
-            <tr><th>age: </th><td><input type="text" name="age"></td></tr>
+            <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+            <tr><th>mail: </th><td><input type="mail" name="mail" value="{{old('mail')}}"></td></tr>
+            <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
             <tr><th><input type="submit" value="send"></th></tr>
         </table>
     </form>

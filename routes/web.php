5e1b58cbec851c0bbe4f58a64a3fc65902ce80ac
shoi <tash0531@icloud.com>
2021-08-14 13:21:23 +0900

    add PersonController

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
 use App\Http\Middleware\HelloMiddleware;
+use App\Http\Controllers\PersonController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +29,4 @@ Route::post('/hello/edit', [HelloController::class, 'update']);
 Route::get('/hello/del', [HelloController::class, 'del']);
 Route::post('/hello/del', [HelloController::class, 'remove']);
 Route::get('/hello/show', [HelloController::class, 'show']);
+Route::get('person', [PersonController::class, 'index']);

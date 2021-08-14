0032d3a421c43f4a71c1909d96406bf4cdb997a6
shoi <tash0531@icloud.com>
2021-08-14 09:02:50 +0900

    fix update function

--- resources/views/hello/edit.blade.php
+++ resources/views/hello/edit.blade.php
@@ -10,13 +10,16 @@
 @section('content')
     <form action="/hello/edit" method="post">
         <table>
-            @CSRF
+            @csrf
             <input type="hidden" name="id" value="{{$form->id}}">
             <tr><th>name: </th><td><input type="text" name="name" valeu="{{$form->name}}"></td></tr>
             <tr><th>mail: </th><td><input type="text" name="mail" valeu="{{$form->mail}}"></td></tr>
             <tr><th>age: </th><td><input type="text" name="age" valeu="{{$form->age}}"></td></tr>
             <tr><th></th><td><input type="submit" valeu="send"></td></tr>
         </table>
+        <p>{{$form->name}}</p>
+        <p>{{$form->mail}}</p>
+        <p>{{$form->age}}</p>
     </form>
 @endsection
 
